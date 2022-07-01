462c6dd6d49f4b8f69d7149b8d2129571a93b770
grilledcheeseplease <lacynunnally402@gmail.com>
2022-06-30 20:52:26 -0400

    fixed tag-routes and routes index.js

--- models/index.js
+++ models/index.js
@@ -7,26 +7,26 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE'
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
+
+  through: ProductTag,
+  foreignKey: 'product_id',
+
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
+Tag.belongsToMany(Product, {
+
+  through: ProductTag,
+  foreignKey: 'tag_id',
+
 });
 
 module.exports = {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,8 +8,12 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }]
+      include: {
+        model: Product,
+        through: ProductTag
+      }
     });
+
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -21,7 +25,10 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, { 
-      include: [{ model: Product }]
+      include: {
+        model: Product,
+        through: ProductTag
+      }
     });
     
     if(!tagData) {
@@ -52,11 +59,8 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       },
     });
-    if (!tagData[0]) {
-      res.status(404).json({ message: 'No tag found with this id!' });
-      return;
-    }
     res.status(200).json(tagData);
+    
   } catch (err) {
     res.status(500).json(err);
   }
